c2260fab3ef8eabf67fd7901312408be4a6756ea
Peter Verswyvelen <bugfact@gmail.com>
2019-02-13 20:29:31 +0100

    Added helper methods to print with different console colors per trace level. Could also use .NET Core logging for this...

---
+++ webrtc-dotnet/TraceLevelExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace WonderMediaProductions.WebRtc
+{
+    public static class TraceLevelExtensions
+    {
+        public static ConsoleColor ToConsoleColor(this TraceLevel level)
+        {
+            switch (level)
+            {
+                case TraceLevel.Error:
+                    return ConsoleColor.Red;
+                case TraceLevel.Info:
+                    return ConsoleColor.DarkCyan;
+                case TraceLevel.Verbose:
+                    return ConsoleColor.DarkGreen;
+                case TraceLevel.Warning:
+                    return ConsoleColor.Yellow;
+                default:
+                    return ConsoleColor.DarkCyan;
+            }
+        }
+
+        public static void WriteToConsole(this TraceLevel level, string line)
+        {
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = ToConsoleColor(level);
+            Console.WriteLine($"[WebRTC {level:G}]:\t${line}");
+            Console.ForegroundColor = color;
+        }
+    }
+}
